59a0b1400bd7c65e902f36f92006b9040c2523b4
simsta6 <51540204+simsta6@users.noreply.github.com>
2022-04-24 23:17:42 +0300

    Button test update

--- src/tests/Components/Buttons/Button.test.tsx
+++ src/tests/Components/Buttons/Button.test.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react-native";
 import { Button } from "../../../components/Buttons/Button";
+import * as hooks from "../../../hooks/colorSchemeHooks";
+import { Colors } from "../../../constants/themes";
 
 describe("<Button>", () => {
+
+    beforeAll(() => {
+        jest.spyOn(hooks, "useAppColors").mockImplementation(() => ({
+            colors: {
+                disabledButton: "black",
+                button: "white",
+                disabledText: "orange",
+                text: "purple",
+            } as Colors
+        }));
+    });
+
     test("First Test", () => {
         const mockFn = jest.fn();
 
-        const { getByTestId } = render(
-            <Button title="Test" onPress={mockFn}></Button>
+        const { getByTestId, getByText, toJSON } = render(
+            <Button title="ButtonTestText" onPress={mockFn}></Button>
         );
 
         fireEvent.press(getByTestId("ButtonTouchableOpacity"));
 
         expect(mockFn).toBeCalled;
+        expect(getByText("ButtonTestText")).toBeTruthy;
+        expect(toJSON()).toMatchSnapshot();
     });
 });
